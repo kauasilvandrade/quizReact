8f6ae1e9f7b85e0f81810138065ebeb1352c87d0
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-04-10 13:01:17 -0300

    Layout de páginas terminado

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,8 +2,17 @@ import { Outlet } from "react-router";
 
 export function Layout() {
   return (
-    <div>
-      <Outlet />
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col">
+        <header>
+          <h1 className="text-5xl font-bold mb-8">
+            Questionário sobre React :)
+          </h1>
+        </header>
+        <main className="bg-white">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
--- src/route/AppRouter.tsx
+++ src/route/AppRouter.tsx
@@ -6,16 +6,22 @@ import { Question3 } from "../pages/Question3";
 import { Question4 } from "../pages/Question4";
 import { Result } from "../pages/Result";
 import { NotFound } from "../pages/NotFound";
+import { Layout } from "../layouts/Layout";
 
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
-      <Route path="/question1" element={<Question1 />} />
-      <Route path="/question2" element={<Question2 />} />
-      <Route path="/question3" element={<Question3 />} />
-      <Route path="/question4" element={<Question4 />} />
+
+      <Route path="/" element={<Layout />}>
+        <Route path="/question1" element={<Question1 />} />
+        <Route path="/question2" element={<Question2 />} />
+        <Route path="/question3" element={<Question3 />} />
+        <Route path="/question4" element={<Question4 />} />
+      </Route>
+
       <Route path="/result" element={<Result />} />
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
