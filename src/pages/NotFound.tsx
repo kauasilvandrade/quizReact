2c85e867a65a888b8d224270d37266207f90b256
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-04-10 12:49:06 -0300

    Página de Não Encontado terminada

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,9 @@
 export function NotFound() {
   return (
-    <div>
-      <h1>Não Encontrado</h1>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-5xl font-bold mb-8">Não Encontrado :(</h1>
+      </div>
     </div>
   );
 }
